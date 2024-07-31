21a874b4b23091620b1bf8fce7306658ac3b821d
Ratt27 <150338584+Ratt27@users.noreply.github.com>
2024-07-31 20:12:38 +0530

    Add files via upload

---
+++ Initials.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Initials {
+    public static void main(String[] args) {
+        String s = "Mohan Das Karam Chand Gandhi";
+        while (s.length() > 0) {
+            s = s.trim();
+            int n = s.indexOf(' ');
+            if (n == -1) {
+                
+                n = s.length();
+            }
+            String w = s.substring(0, 1); // Get the first letter of the current word
+            s = s.substring(n); // Get the rest of the string after the current word
+            System.out.print(w); // Print the initial
+            s = s.trim(); // Trim any leading spaces for the next iteration
+        }
+        System.out.println(); // Print a new line at the end
+    }
+}
