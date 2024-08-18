7a1ed976d1aec7249b619b7b436bf7449dab31df
Ratt27 <150338584+Ratt27@users.noreply.github.com>
2024-08-18 20:50:40 +0530

    Add files via upload

---
+++ ArrayLists.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Collections;
+class ArrayLists
+{
+public static void main(String args[])
+{
+ArrayList<Integer>list=new ArrayList<Integer>();
+//add elements
+list.add(0);
+list.add(2);
+list.add(3);
+System.out.println(list);
+//get elements
+int element=list.get(0);
+System.out.println(element);
+
+// to add elements in between
+list.add(1,1);
+System.out.println(list);
+
+//set element
+list.set(0,5);
+System.out.println(list);
+
+//delete element
+list.remove(3);
+System.out.println(list);
+
+//no of elements in array
+int size=list.size();
+System.out.println(size);
+
+
+//loops
+for(int i=0;i<list.size();i++)
+{
+System.out.print(list.get(i));
+}
+System.out.println();
+
+//sorting
+Collections.sort(list);//here Collection is a class and sort is a function
+System.out.println(list);
+
+}
+}
